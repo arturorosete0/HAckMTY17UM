56d1c45ff10824e07c8be00be27f48d8d92e0ed7
Arturo Rosete <arturorosete0@gmail.com>
2017-08-27 05:35:51 -0500

    agregadas funciones contador de pasos

--- test_arduino/test_arduino.ino
+++ test_arduino/test_arduino.ino
@@ -4,48 +4,97 @@
  * arturorosete0@gmail.com
 */
 
+#include <CurieBLE.h>
 #include <BMI160.h>
 #include <CurieIMU.h>
 
-boolean blinkState = false;          // state of the LED
+long lastStepCount = 0;              // step count on previous polling check
+
+
+BLEPeripheral blePeripheral;
+BLEService heartRateService("180D"); //uuid, standar
+
+BLECharacteristic heartRateChar("2A37", BLERead | BLENotify, 2);
+
+int oldHeartRate = 0;
+long previousMillis = 0;
 
 void setup() {
   Serial.begin(9600); // initialize Serial communication
-  while(!Serial) ;    // wait for serial port to connect..
+  while(!Serial);
+  
+  //Configuración del BLE
+  blePeripheral.setLocalName("UM");
+  blePeripheral.setAdvertisedServiceUuid(heartRateService.uuid());
+  blePeripheral.addAttribute(heartRateService);   // Add the BLE Heart Rate service
+  blePeripheral.addAttribute(heartRateChar); // add HeartRateMeasurement characteristic
+
+  blePeripheral.begin(); //ACtiva el bluetooth
+  Serial.println("Bluetooth active, waiting for connections...");
+  
   /* Initialise the IMU */
   CurieIMU.begin();
   CurieIMU.attachInterrupt(eventCallback);
 
   /* Enable Shock Detection */
-  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, 16000); // 1.5g = 1500 mg
+  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, 16000); // 16g = 16000mg
   CurieIMU.setDetectionDuration(CURIE_IMU_SHOCK, 75);   // 50ms
   CurieIMU.interrupts(CURIE_IMU_SHOCK);
+
+  /*Enable Step detection*/
+  CurieIMU.setStepDetectionMode(CURIE_IMU_STEP_MODE_NORMAL); 
+  CurieIMU.setStepCountEnabled(true);  
 
   Serial.println("IMU initialisation complete, waiting for events...");
 }
 
 void loop() {
-  // blink the LED in the main loop:
-  digitalWrite(13, blinkState);
-  blinkState = !blinkState;
-  delay(200);
+  
+  BLECentral central = blePeripheral.central();
+
+  //Si se ha conectado algun dispositivo
+  if(central){
+    Serial.println("Dispositivo conectado");
+    Serial.println(central.address());
+    digitalWrite(13,HIGH);    
+    
+    while (central.connected()) {
+      updateStepCount();
+      long currentMillis = millis();
+      // if 200ms have passed, check the heart rate measurement:
+      if (currentMillis - previousMillis >= 200) {
+        previousMillis = currentMillis;
+        updateHeartRate(); //Actualiza el ritmo cardiaco
+      }
+    }
+
+    digitalWrite(13,LOW);
+    Serial.println("Disconnected from device...");
+    Serial.println(central.address());
+  }
 }
 
-static void eventCallback(void)
-{
+void updateHeartRate(){
+  //Calcular RPM
+}
+
+static void updateStepCount() {
+  // get the step count:
+  int stepCount = CurieIMU.getStepCount();
+
+  // if the step count has changed, print it:
+    if (stepCount != lastStepCount) {
+    Serial.print("Step count: ");
+    Serial.println(stepCount);
+    // save the current count for comparison next check:
+    lastStepCount = stepCount;
+  }
+}
+
+static void eventCallback(void){
   if (CurieIMU.getInterruptStatus(CURIE_IMU_SHOCK)) {
-    if (CurieIMU.shockDetected(X_AXIS, POSITIVE))
-      Serial.println("Negative shock detected on X-axis");
-    if (CurieIMU.shockDetected(X_AXIS, NEGATIVE))
-      Serial.println("Positive shock detected on X-axis");
-    if (CurieIMU.shockDetected(Y_AXIS, POSITIVE))
-      Serial.println("Negative shock detected on Y-axis");
-    if (CurieIMU.shockDetected(Y_AXIS, NEGATIVE))
-      Serial.println("Positive shock detected on Y-axis");
-    if (CurieIMU.shockDetected(Z_AXIS, POSITIVE))
-      Serial.println("Negative shock detected on Z-axis");
-    if (CurieIMU.shockDetected(Z_AXIS, NEGATIVE))
-      Serial.println("Positive shock detected on Z-axis");
+    //Escribir en BLE "Caída"
+    Serial.println("Impacto");
   }
 }
 
