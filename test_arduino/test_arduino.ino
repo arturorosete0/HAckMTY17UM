efb293d1d2ea21c10305e20e864f8f287fbf1139
Arturo Rosete <arturorosete0@gmail.com>
2017-08-26 23:10:50 -0500

    skecth para detectar golpes

--- test_arduino/test_arduino.ino
+++ test_arduino/test_arduino.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <BMI160.h>
+#include <CurieIMU.h>
 
+/*Prueba de las funciones del arduino 101
+ * Double tap detector
+ * Arturo Rosete, Montemorelos University
+ * arturorosete0@gmail.com
+*/
+
+boolean blinkState = false;          // state of the LED
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  while(!Serial) ;    // wait for serial port to connect..
+  /* Initialise the IMU */
+  CurieIMU.begin();
+  CurieIMU.attachInterrupt(eventCallback);
+
+  /* Enable Shock Detection */
+  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, 1500); // 1.5g = 1500 mg
+  CurieIMU.setDetectionDuration(CURIE_IMU_SHOCK, 50);   // 50ms
+  CurieIMU.interrupts(CURIE_IMU_SHOCK);
+
+  Serial.println("IMU initialisation complete, waiting for events...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // blink the LED in the main loop:
+  digitalWrite(13, blinkState);
+  blinkState = !blinkState;
+  delay(500);
 }
+
+
+static void eventCallback(void)
+{
+  if (CurieIMU.getInterruptStatus(CURIE_IMU_SHOCK)) {
+    if (CurieIMU.shockDetected(X_AXIS, POSITIVE))
+      Serial.println("Negative shock detected on X-axis");
+    if (CurieIMU.shockDetected(X_AXIS, NEGATIVE))
+      Serial.println("Positive shock detected on X-axis");
+    if (CurieIMU.shockDetected(Y_AXIS, POSITIVE))
+      Serial.println("Negative shock detected on Y-axis");
+    if (CurieIMU.shockDetected(Y_AXIS, NEGATIVE))
+      Serial.println("Positive shock detected on Y-axis");
+    if (CurieIMU.shockDetected(Z_AXIS, POSITIVE))
+      Serial.println("Negative shock detected on Z-axis");
+    if (CurieIMU.shockDetected(Z_AXIS, NEGATIVE))
+      Serial.println("Positive shock detected on Z-axis");
+  }
+}
+
